b9c8f920ed5b4cea6b652b6db3e5ef7ebdb38031
meloduarte <devscustomers@gmail.com>
2025-11-07 12:26:41 -0500

    modelos de datos

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI, HTTPException, Body
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Optional, List
 
 app = FastAPI()
 app.title = "Mi primera API con FastAPI"
 app.version = "1.0.0"
 
+class Movie(BaseModel):
+    id: int
+    title:str
+    overview:str
+    year:int
+    rating:float
+    category:str
+
+class MovieUpdate(BaseModel):
+    title:str
+    overview:str
+    year:int
+    rating:float
+    category:str
+    
 movies = [
     {
         "id": 1,
@@ -20,8 +38,17 @@ movies = [
         "year": 2008,
         "rating": 9.0,
         "category": "Action"
+    },
+    {
+    "id": 3,
+    "title": "Nueva Película",
+    "overview": "Descripción de la nueva película",
+    "year": 2023,
+    "rating": 8.5,
+    "category": "Action"
     }
 ]
+
 
 #METODO GET
 @app.get("/home", tags=["Home"])
@@ -29,11 +56,11 @@ def home():
     return "Hola mundo"
 
 @app.get("/movies", tags=["Movies"])
-def get_movies():
+def get_movies() -> List[Movie]:
     return movies
 
 @app.get('/movies/{id}', tags=["Movies"])
-def get_movie(id: int):
+def get_movie(id: int) -> Movie:
     for movie in movies:
         if movie['id'] == id:
             return movie
@@ -41,7 +68,7 @@ def get_movie(id: int):
     raise HTTPException(status_code=404, detail="Movie not found")
 
 @app.get('/movies/', tags=["Movies"])
-def get_movi_by_category(category: str):
+def get_movie_by_category(category: str) -> Movie:
     for movie in movies:
         if movie['category'] == category:
             return movie
@@ -50,47 +77,26 @@ def get_movi_by_category(category: str):
 
 #METODO POST
 @app.post('/movies', tags=["Movies"])
-def create_movie(
-    id: int = Body(), 
-    title: str = Body(), 
-    overview: str = Body(), 
-    year: int =Body(), 
-    rating: float = Body(), 
-    category: str = Body()
-    ):
-    movies.append({
-        "id": id,
-        "title": title,
-        "overview": overview,
-        "year": year,
-        "rating": rating,
-        "category": category
-    })
+def create_movie(movie: Movie) -> List[Movie]:
+    movies.append(movie.model_dump())
     return movies
 
 #METODO PUT
 @app.put('/movies/{id}', tags=["Movies"])
-def update_movie(
-    id: int,
-    title: str = Body(), 
-    overview: str = Body(), 
-    year: int =Body(), 
-    rating: float = Body(), 
-    category: str = Body()
-):
-    for movie in movies:
-        if movie['id'] == id:
-            movie['title'] = title
-            movie['overview'] = overview
-            movie['year'] = year
-            movie['rating'] = rating
-            movie['category'] = category
+def update_movie(id: int, movie: MovieUpdate) -> List[Movie]:
+    for item in movies:
+        if item['id'] == id:
+            item['title'] = movie.title
+            item['overview'] = movie.overview
+            item['year'] = movie.year
+            item['rating'] = movie.rating
+            item['category'] = movie.category
             return movies
     raise HTTPException(status_code=404, detail="Movie not found")
 
 #METODO DELETE
 @app.delete('/movies/{id}', tags=["Movies"])
-def delete_movie(id: int):
+def delete_movie(id: int) -> List[Movie]:
     for movie in movies:
         if movie['id'] == id:
             movies.remove(movie)
